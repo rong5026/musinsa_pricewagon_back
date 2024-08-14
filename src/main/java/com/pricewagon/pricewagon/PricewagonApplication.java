a84ddd91cd32dd4ac3535f205eea1417ae63dc0b
rong5026 <rong5026@naver.com>
2024-08-14 16:08:30 +0900

    :memo: init

---
+++ src/main/java/com/pricewagon/pricewagon/PricewagonApplication.java
@@ -0,0 +1,13 @@
+package com.pricewagon.pricewagon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricewagonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PricewagonApplication.class, args);
+	}
+
+}
