ff2721d89e0e4b24d81e602d9201e935186fc9d0
rong5026 <rong5026@naver.com>
2024-08-26 20:02:16 +0900

    :sparkles: 카테고리 엔티티

---
+++ src/main/java/com/pricewagon/pricewagon/domain/category/entity/Category.java
@@ -0,0 +1,34 @@
+package com.pricewagon.pricewagon.domain.category.entity;
+
+import org.hibernate.annotations.Comment;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Category {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "parent_category_id")
+	@Comment("부모 카테고리 ID")
+	Category parentCategory;
+
+	@Comment("카테고리 이름")
+	@Column(length = 50, nullable = false)
+	private String categoryName;
+
+}
