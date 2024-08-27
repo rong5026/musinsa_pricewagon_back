4f687220ca3fe01ec76680b5bb8228320db2a57c
rong5026 <rong5026@naver.com>
2024-08-27 21:15:05 +0900

    :art: 상품조회 시 필요한 데이터만 담긴 DTO

---
+++ src/main/java/com/pricewagon/pricewagon/domain/product/dto/response/BasicProductInfo.java
@@ -0,0 +1,38 @@
+package com.pricewagon.pricewagon.domain.product.dto.response;
+
+import com.pricewagon.pricewagon.domain.product.entity.Product;
+import com.pricewagon.pricewagon.domain.product.entity.ProductHistory;
+import com.pricewagon.pricewagon.domain.product.entity.ShopType;
+
+public record BasicProductInfo(
+	 Long id,
+	 String name,
+	 String brand,
+	 Double starScore,
+	 Integer reviewCount,
+	 Integer likeCount,
+	 String imgUrl,
+	 ShopType shopType,
+	 Integer originPrice,
+	 Integer salePrice,
+	 String categoryName,
+	 String parentCategoryName
+
+) {
+	public static BasicProductInfo of (Product product, ProductHistory productHistory) {
+		return new BasicProductInfo(
+			product.getId(),
+			product.getName(),
+			product.getBrand(),
+			product.getStarScore(),
+			product.getReviewCount(),
+			product.getLikeCount(),
+			product.getImgUrl(),
+			product.getShopType(),
+			product.getOriginPrice(),
+			productHistory.getPrice(),
+			product.getCategory() != null ? product.getCategory().getCategoryName() : null ,
+			product.getCategory().getParentCategory() != null ? product.getCategory().getParentCategory().getCategoryName() : null
+		);
+	}
+}
