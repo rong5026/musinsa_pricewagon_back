8fa1ed3dfcceca1c419cd273915d664c1ef5c5cc
rong5026 <rong5026@naver.com>
2024-08-27 17:43:00 +0900

    :art: Product, ProductDetail 엔티티 수정

--- src/main/java/com/pricewagon/pricewagon/domain/product/entity/ProductDetail.java
+++ src/main/java/com/pricewagon/pricewagon/domain/product/entity/ProductDetail.java
@@ -4,8 +4,6 @@ import org.hibernate.annotations.Comment;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -29,17 +27,19 @@ public class ProductDetail {
 	@JoinColumn(name = "product_id")
 	private Product product;
 
-	@Comment("쇼핑몰 타입")
-	@Enumerated(EnumType.STRING)
-	@Column(length = 50)
-	private ShopType shopType;
-
-	@Comment("좋아요 수")
+	@Comment("최고가")
 	@Column(nullable = false)
-	private Integer likeCount;
+	private Integer highPrice;
 
-	@Comment("구매 수")
+	@Comment("평균가")
 	@Column(nullable = false)
-	private Integer purchaseCount;
+	private Integer middlePrice;
 
+	@Comment("최저가")
+	@Column(nullable = false)
+	private Integer lowPrice;
+
+	@Comment("상품 URL")
+	@Column(length = 200, nullable = false, unique = true)
+	private String productUrl;
 }
