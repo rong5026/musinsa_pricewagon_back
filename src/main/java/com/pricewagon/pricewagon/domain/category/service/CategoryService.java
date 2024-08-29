267b3f4230b041bebe3f97749e9727763524e1c2
rong5026 <rong5026@naver.com>
2024-08-29 19:49:41 +0900

    :fire: 사용안하는 코드 삭제

--- src/main/java/com/pricewagon/pricewagon/domain/category/service/CategoryService.java
+++ src/main/java/com/pricewagon/pricewagon/domain/category/service/CategoryService.java
@@ -1,12 +1,7 @@
 package com.pricewagon.pricewagon.domain.category.service;
 
-import java.util.List;
-
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import com.pricewagon.pricewagon.domain.category.entity.Category;
-import com.pricewagon.pricewagon.domain.category.repository.CategoryRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,9 +9,5 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Transactional
 public class CategoryService {
-	private final CategoryRepository categoryRepository;
 
-	public List<Category> getCategoriesByParentCategoryId(Long categoryId) {
-		return categoryRepository.findByParentCategory_Id(categoryId);
-	}
 }
