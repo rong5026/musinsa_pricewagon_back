8f04e5f0e473f44c99c528109ed86426502560f3
rong5026 <rong5026@naver.com>
2024-09-04 18:20:17 +0900

    :zap: githubaction 실행까지 테스트

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,22 +1,23 @@
-REPOSITORY=/app
-cd $REPOSITORY
+#!/bin/bash
 
-APP_NAME=SNAP
-JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
-JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+APP_NAME=PRICEWAGON
+REPOSITORY=/home/hong/app/pricewagon-back
+JAR_NAME=$(ls $REPOSITORY | grep '.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/$JAR_NAME
 
+# 현재 실행 중인 애플리케이션 PID 확인
 CURRENT_PID=$(pgrep -f $APP_NAME)
 
-if [ -z $CURRENT_PID ] #2
+if [ -z $CURRENT_PID ]
 then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다!."
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
   echo "> kill -15 $CURRENT_PID"
   sudo kill -15 $CURRENT_PID
   sleep 5
 fi
 
-echo "> $JAR_PATH 배포" #3
+echo "> $JAR_PATH 배포"
 nohup java -jar \
-        -Dspring.profiles.active=dev \
-        build/libs/$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+  -Dspring.profiles.active=prod \
+  $JAR_PATH > $REPOSITORY/nohup.out 2>&1 &
