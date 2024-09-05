6f5ee32e61f7c995329b0e4033d291561e5e946b
rong5026 <rong5026@naver.com>
2024-09-05 20:44:31 +0900

    blue green 적용

--- scripts/blue-green-deploy.sh
+++ scripts/blue-green-deploy.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-# 작업 디렉토리를 /home/ubuntu/app으로 변경
-#cd /home/ubuntu/bluegreenapp
+# 작업 디렉토리 설정
+cd /home/hong/app/pricewagon-back
 
-cd /home/hong/app/bluegreenapp
-# 환경변수 DOCKER_APP_NAME을 animal-meeting으로 설정
-DOCKER_APP_NAME=animal-meeting-blue-green
+DOCKER_APP_NAME=pricewagon-blue-green
+
+DEPLOY_LOG="/home/hong/app/blue-grenn-deploy.log"  # 로그 파일 경로를 변수로 설정
 
 # 실행중인 blue가 있는지 확인
 # 프로젝트의 실행 중인 컨테이너를 확인하고, 해당 컨테이너가 실행 중인지 여부를 EXIST_BLUE 변수에 저장
 EXIST_BLUE=$(sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
 
 # 배포 시작한 날짜와 시간을 기록
-echo "배포 시작일자 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/hong/app/deploy.log
+echo "배포 시작일자 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> $DEPLOY_LOG
 
 # green이 실행중이면 blue up
 # EXIST_BLUE 변수가 비어있는지 확인
 if [ -z "$EXIST_BLUE" ]; then
 
   # 로그 파일(/home/ubuntu/deploy.log)에 "blue up - blue 배포 : port:8081"이라는 내용을 추가
-  echo "blue 배포 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/hong/app/deploy.log
+  echo "blue 배포 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >>  $DEPLOY_LOG
 
 	# docker-compose.blue.yml 파일을 사용하여 spring-blue 프로젝트의 컨테이너를 빌드하고 실행
 	sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --build
@@ -33,13 +33,13 @@ if [ -z "$EXIST_BLUE" ]; then
   # blue가 현재 실행중이지 않다면 -> 런타임 에러 또는 다른 이유로 배포가 되지 못한 상태
   if [ -z "$BLUE_HEALTH" ]; then
     # slack으로 알람을 보낼 수 있는 스크립트를 실행한다.
-     echo "blue 배포 도중 실패 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/hong/app/deploy.log
+     echo "blue 배포 도중 실패 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> $DEPLOY_LOG
 
   # blue가 현재 실행되고 있는 경우에만 green을 종료
   else
 
     # /home/ubuntu/deploy.log: 로그 파일에 "green 중단 시작"이라는 내용을 추가
-    echo "green 중단 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/hong/app/deploy.log
+    echo "green 중단 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> $DEPLOY_LOG
 
     # docker-compose.green.yml 파일을 사용하여 spring-green 프로젝트의 컨테이너를 중지
     sudo docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
@@ -47,12 +47,12 @@ if [ -z "$EXIST_BLUE" ]; then
     # 사용하지 않는 이미지 삭제
     sudo docker image prune -af
 
-    echo "green 중단 완료 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/hong/app/deploy.log
+    echo "green 중단 완료 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >>  $DEPLOY_LOG
   fi
 
 # blue가 실행중이면 green up
 else
-	echo "green 배포 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/hong/app/deploy.log
+	echo "green 배포 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >>  $DEPLOY_LOG
 	sudo docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d --build
 
   sleep 30
@@ -61,11 +61,11 @@ else
 
   if [ -z "$GREEN_HEALTH" ]; then
 
-      echo "green 배포 도중 실패 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/hong/app/deploy.log
+      echo "green 배포 도중 실패 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> $DEPLOY_LOG
   else
 
       # /home/ubuntu/deploy.log: 로그 파일에 "blue 중단 시작"이라는 내용을 추가
-      echo "blue 중단 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/hong/app/deploy.log
+      echo "blue 중단 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> $DEPLOY_LOG
 
       # docker-compose.blue.yml 파일을 사용하여 spring-green 프로젝트의 컨테이너를 중지
       sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
@@ -73,11 +73,11 @@ else
       # 사용하지 않는 이미지 삭제
       sudo docker image prune -af
 
-      echo "blue 중단 완료 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/hong/app/deploy.log
+      echo "blue 중단 완료 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> $DEPLOY_LOG
   fi
 
 fi
-  echo "배포 종료  : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/hong/app/deploy.log
+  echo "배포 종료  : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> $DEPLOY_LOG
 
-  echo "===================== 배포 완료 =====================" >> /home/hong/app/deploy.log
-  echo >> /home/hong/app/deploy.log
+  echo "===================== 배포 완료 =====================" >>  $DEPLOY_LOG
+  echo >>  $DEPLOY_LOG
