0f4416114867d02d924a6b8c0beb209835c9b46c
rong5026 <rong5026@naver.com>
2024-09-05 20:35:31 +0900

    docker compose init

--- src/main/java/com/pricewagon/pricewagon/domain/product/service/ProductHistoryService.java
+++ src/main/java/com/pricewagon/pricewagon/domain/product/service/ProductHistoryService.java
@@ -1,5 +1,7 @@
 package com.pricewagon.pricewagon.domain.product.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,9 +25,10 @@ public class ProductHistoryService {
 	public Integer getDistinctOrLatestPriceByProductId(Long productId) {
 		ProductHistory latestHistory = getLatestHistoryByProductId(productId);
 
+		Page<ProductHistory> result = productHistoryRepository.findFirstByProductIdAndPriceNot(productId,  latestHistory.getPrice(), PageRequest.of(0, 1));
+		ProductHistory previousHistory = result.getContent().isEmpty() ? latestHistory : result.getContent().get(0);
+
 		// 최근 값과 다른 첫 번째 값을 찾고, 없으면 최근 값을 리턴
-		return productHistoryRepository.findFirstByProductIdAndPriceNot(productId, latestHistory.getPrice())
-			.map(ProductHistory::getPrice)
-			.orElse(latestHistory.getPrice());
+		return previousHistory.getPrice();
 	}
 }
