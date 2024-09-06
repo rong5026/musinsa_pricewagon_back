579d10b683b674be325c82df0685686c0e59cae5
rong5026 <rong5026@naver.com>
2024-09-06 17:43:25 +0900

    :bug: container이름 변경 후 test

--- scripts/blue-green-deploy.sh
+++ scripts/blue-green-deploy.sh
@@ -12,9 +12,8 @@ DEPLOY_LOG="/home/hong/app/blue-green-deploy.log"  # 로그 파일 경로를 변
 # 배포 시작한 날짜와 시간을 기록
 echo "배포 시작일자 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> $DEPLOY_LOG
 
-# 실행중인 blue가 있는지 확인
-EXIST_BLUE=$(docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.yml ps spring-blue | grep Up)
-
+#실행중인 blue 서비스가 있는지 확인
+EXIST_BLUE=$(docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.yml ps | grep spring-blue-container | grep Up)
 # 이미 실행 중인 blue가 있으면 종료
 if [ -n "$EXIST_BLUE" ]; then
   echo "이미 실행 중인 blue 컨테이너가 있습니다. 종료 중..." >> $DEPLOY_LOG
@@ -29,7 +28,7 @@ docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.yml up -d --build sp
 # blue 컨테이너 실행 확인
 for i in {1..6}; do
   sleep 7
-  BLUE_HEALTH=$(docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.yml ps spring-blue | grep Up)
+  BLUE_HEALTH=$(docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.yml ps  | grep spring-blue-container | grep Up)
   if [ -n "$BLUE_HEALTH" ]; then
     break
   fi
@@ -50,7 +49,7 @@ docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.yml up -d --build s
 
 for i in {1..6}; do
   sleep 7
-  GREEN_HEALTH=$(docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.yml ps spring-green | grep Up)
+  GREEN_HEALTH=$(docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.yml ps | grep spring-green-container | grep Up)
   if [ -n "$GREEN_HEALTH" ]; then
     break
   fi
