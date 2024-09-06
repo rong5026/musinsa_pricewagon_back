55444a2ecb4e141374f694d47ad25294cbac3da3
rong5026 <rong5026@naver.com>
2024-09-06 18:37:27 +0900

    :bug: nginx, blue/green 분리

--- scripts/blue-green-deploy.sh
+++ scripts/blue-green-deploy.sh
@@ -9,16 +9,6 @@ DOCKER_APP_NAME=pricewagon
 
 DEPLOY_LOG="/home/hong/app/blue-green-deploy.log"  # 로그 파일 경로를 변수로 설정
 
-# Nginx가 이미 실행 중인지 확인
-EXIST_NGINX=$(docker-compose -p ${DOCKER_APP_NAME} -f docker-compose.yml ps | grep nginx-proxy | grep Up)
-
-if [ -z "$EXIST_NGINX" ]; then
-  echo "Nginx 실행" >> $DEPLOY_LOG
-  docker-compose -p ${DOCKER_APP_NAME} -f docker-compose.yml up -d --build nginx
-else
-  echo "Nginx 이미 실행 중" >> $DEPLOY_LOG
-fi
-
 # 실행중인 blue가 있는지 확인
 EXIST_BLUE=$(docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.yml ps | grep spring-blue-container | grep Up)
 
@@ -30,7 +20,7 @@ if [ -z "$EXIST_BLUE" ]; then
   echo "blue 배포 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >>  $DEPLOY_LOG
 
   # blue 배포
-  docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.yml up -d --build spring-blue nginx
+  docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.yml up -d --build spring-blue
 
   # 컨테이너 실행 확인
   for i in {1..6}; do
@@ -53,7 +43,7 @@ if [ -z "$EXIST_BLUE" ]; then
 # blue가 실행중이면 green up
 else
   echo "green 배포 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >>  $DEPLOY_LOG
-  docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.yml up -d --build spring-green nginx
+  docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.yml up -d --build spring-green
 
   for i in {1..6}; do
     sleep 7
