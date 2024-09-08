773a634274e72764864c18050f7e4dadae018196
rong5026 <rong5026@naver.com>
2024-09-08 19:37:30 +0900

    :recycle: originPrice 제거 후 currentPrice로 변경

--- src/main/java/com/pricewagon/pricewagon/domain/product/service/ProductService.java
+++ src/main/java/com/pricewagon/pricewagon/domain/product/service/ProductService.java
@@ -1,9 +1,7 @@
 package com.pricewagon.pricewagon.domain.product.service;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
@@ -16,7 +14,6 @@ import com.pricewagon.pricewagon.domain.category.service.CategoryService;
 import com.pricewagon.pricewagon.domain.product.dto.response.BasicProductInfo;
 import com.pricewagon.pricewagon.domain.product.dto.response.IndividualProductInfo;
 import com.pricewagon.pricewagon.domain.product.entity.Product;
-import com.pricewagon.pricewagon.domain.product.entity.ProductHistory;
 import com.pricewagon.pricewagon.domain.product.entity.ShopType;
 import com.pricewagon.pricewagon.domain.product.repository.ProductRepository;
 
@@ -37,9 +34,8 @@ public class ProductService {
 
 		return products.stream()
 			.map(product -> {
-				ProductHistory latestHistory = productHistoryService.getLatestHistoryByProductId(product.getId());
 				Integer previousPrice = productHistoryService.getDistinctOrLatestPriceByProductId(product.getId());
-				return BasicProductInfo.createHistoryOf(product, latestHistory, previousPrice);
+				return BasicProductInfo.createHistoryOf(product, previousPrice);
 			})
 			.toList();
 	}
@@ -49,15 +45,12 @@ public class ProductService {
 		Product product = productRepository.findByShopTypeAndProductNumber(shopType, productNumber)
 			.orElseThrow(() -> new RuntimeException("존재하지 않는 상품입니다."));
 
-		ProductHistory latestHistory = getLatestProductHistory(product.getProductHistories())
-			.orElseThrow(() -> new RuntimeException("상품 가격 히스토리가 존재하지 않습니다."));
-
 		Category childCategory = product.getCategory();
 		ParentAndChildCategoryDTO parentAndChildCategoryDTO = categoryService
 			.getParentAndChildCategoriesByChildId(childCategory.getId());
 
 		Integer previousPrice = productHistoryService.getDistinctOrLatestPriceByProductId(product.getId());
-		BasicProductInfo basicProductInfo = BasicProductInfo.createHistoryOf(product, latestHistory, previousPrice);
+		BasicProductInfo basicProductInfo = BasicProductInfo.createHistoryOf(product, previousPrice);
 
 		IndividualProductInfo individualProductInfo = IndividualProductInfo.from(product, basicProductInfo, parentAndChildCategoryDTO);
 
@@ -81,15 +74,9 @@ public class ProductService {
 		return productRepository.findByShopTypeAndCategory_IdIn(shopType, categoriesId, pageable)
 			.stream()
 			.map(product -> {
-				ProductHistory latestHistory = productHistoryService.getLatestHistoryByProductId(product.getId());
 				Integer previousPrice = productHistoryService.getDistinctOrLatestPriceByProductId(product.getId());
-				return BasicProductInfo.createHistoryOf(product, latestHistory, previousPrice);
+				return BasicProductInfo.createHistoryOf(product, previousPrice);
 			})
 			.toList();
-	}
-
-	private Optional<ProductHistory> getLatestProductHistory(List<ProductHistory> productHistories) {
-		return productHistories.stream()
-			.max(Comparator.comparing(ProductHistory::getCreatedAt));
 	}
 }
