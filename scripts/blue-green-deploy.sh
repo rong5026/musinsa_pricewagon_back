f6e21a31eb3a793685f3a389310fb4d71e1038e3
rong5026 <rong5026@naver.com>
2024-09-12 15:19:37 +0900

    nginx.template,conf로 대체

--- scripts/blue-green-deploy.sh
+++ scripts/blue-green-deploy.sh
@@ -12,14 +12,16 @@ NGINX_CONFIG="/etc/nginx/nginx.conf"
 # 실행 중인 blue가 있는지 확인
 EXIST_BLUE=$(docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.yml ps | grep spring-blue-container | grep Up)
 
-# 현재 실행 중인 컨테이너가 Blue인지 Green인지 확인하여 Nginx 설정 변경
-if [ -z "$EXIST_BLUE" ]; then
-    # Blue가 실행 중이 아닌 경우 Nginx 설정을 Blue로 변경
-    docker exec nginx-proxy sed -i 's/spring-green-container:8080/spring-blue-container:8080/g' $NGINX_CONFIG
-else
-    # Green이 실행 중인 경우 Nginx 설정을 Green으로 변경
-    docker exec nginx-proxy sed -i 's/spring-blue-container:8080/spring-green-container:8080/g' $NGINX_CONFIG
-fi
+## 현재 실행 중인 컨테이너가 Blue인지 Green인지 확인하여 Nginx 설정 변경
+#if [ -z "$EXIST_BLUE" ]; then
+#    CONTAINER_NAME="spring-blue-container"
+#    # Blue가 실행 중이 아닌 경우 Nginx 설정을 Blue로 변경
+#    # docker exec nginx-proxy sed -i 's/spring-green-container:8080/spring-blue-container:8080/g' $NGINX_CONFIG
+#else
+#    # Green이 실행 중인 경우 Nginx 설정을 Green으로 변경
+##    docker exec nginx-proxy sed -i 's/spring-blue-container:8080/spring-green-container:8080/g' $NGINX_CONFIG
+#fi
+## 템플릿을 기반으로 실제 Nginx 설정 파일 생성
 #docker exec nginx-proxy nginx -s reload
 
 # 배포 시작한 날짜와 시간을 기록
@@ -44,6 +46,9 @@ if [ -z "$EXIST_BLUE" ]; then
   if [ -z "$BLUE_HEALTH" ]; then
     echo "blue 배포 도중 실패 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> $DEPLOY_LOG
   else
+
+    CONTAINER_NAME="spring-blue-container"
+    sed "s/{{CONTAINER_NAME}}/$CONTAINER_NAME/g" /home/hong/app/pricewagon-blue-green/nginx.template.conf > /etc/nginx/nginx.conf
 
     echo "Nginx 리로드 시작일자 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> $DEPLOY_LOG
     docker exec nginx-proxy nginx -s reload
@@ -70,6 +75,9 @@ else
   if [ -z "$GREEN_HEALTH" ]; then
     echo "green 배포 도중 실패 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> $DEPLOY_LOG
   else
+
+    CONTAINER_NAME="spring-green-container"
+    sed "s/{{CONTAINER_NAME}}/$CONTAINER_NAME/g" /home/hong/app/pricewagon-blue-green/nginx.template.conf > /etc/nginx/nginx.conf
 
     # Nginx 재시작 또는 설정 리로드
     echo "Nginx 리로드 시작일자 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> $DEPLOY_LOG
