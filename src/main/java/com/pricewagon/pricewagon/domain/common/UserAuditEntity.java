5d2ce035f8642577bb8fdf31326263e25ab0e8a1
rong5026 <rong5026@naver.com>
2024-09-26 14:01:58 +0900

    :sparkles: 유저용 시간포함 createdAt, updatedAD 구현

---
+++ src/main/java/com/pricewagon/pricewagon/domain/common/UserAuditEntity.java
@@ -0,0 +1,27 @@
+package com.pricewagon.pricewagon.domain.common;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+public class UserAuditEntity {
+
+	@CreatedDate
+	@Column(name = "created_at", nullable = false, updatable = false)
+	private LocalDateTime createdAt;
+
+	@LastModifiedDate
+	@Column(name = "updated_at", nullable = false)
+	private LocalDateTime updatedAt;
+
+}
