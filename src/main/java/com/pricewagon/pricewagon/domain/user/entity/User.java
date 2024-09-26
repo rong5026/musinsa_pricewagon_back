c87bb4999f20ce58cc177493b3d30225677c7350
rong5026 <rong5026@naver.com>
2024-09-26 12:59:19 +0900

    :sparkles: 유저, 알림 엔티티 생성

---
+++ src/main/java/com/pricewagon/pricewagon/domain/user/entity/User.java
@@ -0,0 +1,41 @@
+package com.pricewagon.pricewagon.domain.user.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.annotations.Comment;
+
+import com.pricewagon.pricewagon.domain.alarm.entity.Alarm;
+import com.pricewagon.pricewagon.domain.common.BaseAuditEntity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseAuditEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Comment("아이디")
+	@Column(columnDefinition = "varchar(20)", nullable = false, unique = true)
+	private String account;
+
+	@Comment("비밀번호")
+	@Column(columnDefinition = "varchar(20)", nullable = false)
+	private String password;
+
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Alarm> alarms = new ArrayList<>();
+
+}
