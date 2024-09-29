1efbe5b660334b1893f0e109a61b06c40a2451cb
rong5026 <rong5026@naver.com>
2024-09-29 16:32:12 +0900

    :sparkles: createdAt, updatedAt 생성 되도록 EnableJpaAuditing 설정

--- src/main/java/com/pricewagon/pricewagon/PricewagonApplication.java
+++ src/main/java/com/pricewagon/pricewagon/PricewagonApplication.java
@@ -2,8 +2,10 @@ package com.pricewagon.pricewagon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class PricewagonApplication {
 
 	public static void main(String[] args) {
