62b7ef54785d3dc63c4d887303b4a6d854eb2d72
rong5026 <rong5026@naver.com>
2024-09-30 14:50:23 +0900

    :truck: DTO -> Dto 네이밍 수정

--- src/main/java/com/pricewagon/pricewagon/domain/product/dto/response/IndividualProductInfo.java
+++ src/main/java/com/pricewagon/pricewagon/domain/product/dto/response/IndividualProductInfo.java
@@ -3,20 +3,20 @@ package com.pricewagon.pricewagon.domain.product.dto.response;
 import java.util.List;
 
 import com.pricewagon.pricewagon.domain.category.dto.response.ParentAndChildCategoryDTO;
-import com.pricewagon.pricewagon.domain.product.dto.ProductDetailDTO;
+import com.pricewagon.pricewagon.domain.product.dto.ProductDetailDto;
 import com.pricewagon.pricewagon.domain.product.entity.Product;
 import com.pricewagon.pricewagon.domain.product.entity.ProductHistory;
 
 public record IndividualProductInfo(
 
 	BasicProductInfo basicProductInfo, // 메인 정보
-	ProductDetailDTO productDetail, // 부과 정보
+	ProductDetailDto productDetail, // 부과 정보
 	List<ProductHistory> productHistoryList, // 가격 히스토리
 	ParentAndChildCategoryDTO parentAndChildCategoryDTO // 카테고리
 	) {
 
 	public static IndividualProductInfo from(Product product, BasicProductInfo basicProductInfo, ParentAndChildCategoryDTO parentAndChildCategoryDTO) {
-		ProductDetailDTO productDetail = ProductDetailDTO.toDTO(product.getProductDetail());
+		ProductDetailDto productDetail = ProductDetailDto.toDTO(product.getProductDetail());
 		return new IndividualProductInfo(basicProductInfo, productDetail, product.getProductHistories(), parentAndChildCategoryDTO);
 	}
 }
