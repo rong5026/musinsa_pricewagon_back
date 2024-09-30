32eaf504354e2cd15f5064dcfdf4d66ca095f7b3
rong5026 <rong5026@naver.com>
2024-09-30 14:50:23 +0900

    :truck: DTO -> Dto 네이밍 수정

--- src/main/java/com/pricewagon/pricewagon/domain/product/dto/ProductDetailDto.java
+++ src/main/java/com/pricewagon/pricewagon/domain/product/dto/ProductDetailDto.java
@@ -2,14 +2,14 @@ package com.pricewagon.pricewagon.domain.product.dto;
 
 import com.pricewagon.pricewagon.domain.product.entity.ProductDetail;
 
-public record ProductDetailDTO(
+public record ProductDetailDto(
 	 Integer highPrice,
 	 Integer middlePrice,
 	 Integer lowPrice,
 	 String productUrl
 ) {
-	public static ProductDetailDTO toDTO(ProductDetail productDetail) {
-		return new ProductDetailDTO(
+	public static ProductDetailDto toDTO(ProductDetail productDetail) {
+		return new ProductDetailDto(
 			productDetail.getHighPrice(),
 			productDetail.getMiddlePrice(),
 			productDetail.getLowPrice(),
